fcd1e8cd9380887d5240682105cc52e649673b98
Matthew <mtaylorex@gmail.com>
2023-12-03 16:00:21 -0500

    add a template

---
+++ 2023/solution.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from utils import aoc
+
+"""template"""
+
+TEST = False
+
+data = aoc.getInput(2023, DAY, TEST)
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+print(part1())
+print(part2())
