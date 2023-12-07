e5cc242463397990014abbda51d2ebdcaba0f7ec
Matthew <mtaylorex@gmail.com>
2023-12-06 19:39:04 -0500

    Cleanup

--- 2023/day4/solution.py
+++ 2023/day4/solution.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import defaultdict, deque
 import math
 from utils import aoc
 from utils.aoc import parseInts
@@ -27,31 +27,39 @@ def part1():
 
 def part2():
     total = 0
-    copies = deque()
-    games = {}
+    # copies = deque()
+    # games = {}
+
+    games = defaultdict(int)
     for card in data:
         total += 1
         game, numbers = card.split(":")
         game = int(parseInts(game)[0])
+        games[game] += 1
         winningNumbers, myNumbers = numbers.split("|")
         winningNumbers = set(parseInts(winningNumbers))
         myNumbers = set(parseInts(myNumbers))
-        matches = games.get(game, 0)
-        if not matches:
-            for number in myNumbers:
-                if number in winningNumbers:
-                    matches += 1
-            games[game] = matches
+        # matches = games.get(game, 0)
+        # if not matches:
+        matches = len(winningNumbers & myNumbers)
+        # matches = 0
+        # for number in myNumbers:
+        #     if number in winningNumbers:
+        #         matches += 1
+        # games[game] = matches
 
         for i in range(matches):
-            copies.append(game + i + 1)
-
-    while copies:
-        total += 1
-        copy = copies.popleft()
-        matches = games[copy]
-        for i in range(matches):
-            copies.append(copy + i + 1)
+            # if games + i + 1 not in games.keys():
+            #     games[games + i + 1] = 0
+            games[game + i + 1] += games[game]
+            # copies.append(game + i + 1)
+    return sum(games.values())
+    # while copies:
+    #     total += 1
+    #     copy = copies.popleft()
+    #     matches = games[copy]
+    #     for i in range(matches):
+    #         copies.append(copy + i + 1)
 
     return total
 
