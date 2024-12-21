ec6cdf08f45ef2bd96f08ec125d3f7f85606ca31
Matthew <mtaylorex@gmail.com>
2024-12-20 23:51:13 -0500

    Day 16

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -18,35 +18,24 @@ width, height = len(data[0]), len(data)
 
 for y, line in enumerate(data):
     for x, c in enumerate(line):
-        if c == "#":
-            # grid[x + y*1j] = "#"
-            grid[x, y] = "#"
+        if c != "#":
+            grid[x, y] = c
         if c == "S":
             start = x, y
-            # start = x + y*1j
         if c == "E":
-            # end = x + y*1j
             end = x, y
 
-# def printgrid():
-#     for line in grid:
-#         print(line)
 def printgrid():
     for y in range(height):
-        # print("".join(grid.get(r+c*1j, ".")  for c in range(width)))
-        print("".join(grid.get((x, y), ".") if (x, y) != start else "S" for x in range(width)))
+        print("".join(grid.get((x, y), "#") if (x, y) != start else "S" for x in range(width)))
 
 def inbounds(pos):
     x, y = pos
     return 0 <= x < width and 0 <= y < height
 
-# def inbounds(coord: complex):
-#     return 0 <= coord.real < width and 0 <= coord.imag < height
-
 if TEST:
     printgrid()
 
-# dirs = {"N": - 1j, "E": 1 + 0j, "S": 0 + 1j, "W": -1 + 0j}
 dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))
 N = (0, -1)
 E = (1, 0)
@@ -61,37 +50,41 @@ drunes = {
 }
 
 def dijkstra(start: tuple[int, int], end: tuple[int, int]):
-    heap = [(0, E, start, [])]
-    visited = set()
-    # path = []
+    dist = defaultdict(lambda: 1e9)
+    best = 1e9
+    heap = [(0, E, start, [start])]
+    visited = []
     while heap:
         cost, facing, (ux, uy), path = heappop(heap)
-        if (ux, uy) in visited:
+        if cost > dist[(ux, uy), facing]:
             continue
-        visited.add((ux, uy))
-        if (ux, uy ) == end:
-            return cost, path
+        dist[(ux, uy), facing] = cost
+    
+        if (ux, uy) == end and cost <= best:
+            visited.extend(path)
+            best = cost
+     
         for v, d, in (((ux+dx, uy+dy), (dx, dy)) for (dx, dy) in (N, E, S, W)):
-            c = abs(dirs.index(facing) - dirs.index(d))*1000+1
-            if not inbounds(v) or v in visited or grid.get(v) == "#":
+            c = 1
+            findex, dindex = dirs.index(facing), dirs.index(d)
+            if  abs(dindex-findex) == 1 or abs(dindex-findex) == 3:
+                c += 1000
+            elif abs(dindex-findex) == 2:
+                c += 2000
+            if v not in grid:
                 continue
 
-            heappush(heap, (cost+c, d, v, [*path, (drunes[d], (ux, uy))]))
-                
-    return -1, path
+            heappush(heap, (cost+c, d, v, [*path, (ux, uy)]))
+    
+    return best, path, visited
 
 
-
-pt1, path = dijkstra(start, end)
-for (rune, (x, y)) in path:
-    grid[x, y] = rune
+pt1, path, visited = dijkstra(start, end)
 
 if TEST or True:
     printgrid()
     print(len(path))
 
 print("pt1", pt1)
-
-pt2 = 0
-print(pt2)
+print("pt2", len(set(visited))+1)
 
